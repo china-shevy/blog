ceb70372a4a4aae3abd897a512cb637e10bd61d6
shevy <18600686927@163.com>
2015-11-07 14:27:28 +0800

    add blog

---
+++ models/Comment.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_comment".
+ *
+ * @property integer $id
+ * @property string $content
+ * @property integer $status
+ * @property integer $create_time
+ * @property string $author
+ * @property string $email
+ * @property string $url
+ * @property integer $post_id
+ *
+ * @property TblPost $post
+ */
+class Comment extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_comment';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['content', 'status', 'author', 'email', 'post_id'], 'required'],
+            [['content'], 'string'],
+            [['status', 'create_time', 'post_id'], 'integer'],
+            [['author', 'email', 'url'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'content' => 'Content',
+            'status' => 'Status',
+            'create_time' => 'Create Time',
+            'author' => 'Author',
+            'email' => 'Email',
+            'url' => 'Url',
+            'post_id' => 'Post ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPost()
+    {
+        return $this->hasOne(TblPost::className(), ['id' => 'post_id']);
+    }
+}
---
+++ models/Post.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_post".
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $content
+ * @property string $tags
+ * @property integer $status
+ * @property integer $create_time
+ * @property integer $update_time
+ * @property integer $author_id
+ *
+ * @property TblComment[] $tblComments
+ * @property TblUser $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'tbl_post';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'content', 'status', 'author_id'], 'required'],
+            [['content', 'tags'], 'string'],
+            [['status', 'create_time', 'update_time', 'author_id'], 'integer'],
+            [['title'], 'string', 'max' => 128]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'content' => 'Content',
+            'tags' => 'Tags',
+            'status' => 'Status',
+            'create_time' => 'Create Time',
+            'update_time' => 'Update Time',
+            'author_id' => 'Author ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTblComments()
+    {
+        return $this->hasMany(TblComment::className(), ['post_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(TblUser::className(), ['id' => 'author_id']);
+    }
+}
